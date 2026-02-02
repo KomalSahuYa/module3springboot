5c877cd62c5d61ab665b31c6b00ad96b835a5bf9
Komal Sahu <VE00YN373@YMI.COM>
2026-02-02 05:30:44 +0530

    Assignment 2 added

---
+++ assignment2-springboot-basics-2/src/main/java/com/app/book/Assignment2SpringbootBasics2Application.java
@@ -0,0 +1,13 @@
+package com.app.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2SpringbootBasics2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2SpringbootBasics2Application.class, args);
+	}
+
+}
